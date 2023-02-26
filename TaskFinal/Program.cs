1009aafc01fb798953cc1c3f352a593767159751
jntGCC <demiteli.png.@gmail.com>
2023-02-26 21:01:50 +0300

    Задача решена

--- TaskFinal/Program.cs
+++ TaskFinal/Program.cs
@@ -2,36 +2,39 @@
 
 int ArrayCheck(string[] array)
 {
- int count = 0;
- for (int i = 0; i < array.Length; i++)
-    {
- if (array[i].Length <= 3) count++;
-    }
- return count;
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i].Length <= 3) 
+      {
+         count++;
+      }
+   }
+   return count;
 }
 
 string[] FillResultArray(string[] array, int count)
 {
- string[] result = new string[count];
- for (int i = 0; i < array.Length; i++)
-    {
- if (array[i].Length <= 3)
-        {
- result[result.Length - count] = array[i];
- count--;
-        }
-    }
- return result;
+   string[] result = new string[count];
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i].Length <= 3)
+      {
+         result[result.Length - count] = array[i];
+         count--;
+      }
+   }
+   return result;
 }
 
 void PrintArray(string[] array)
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i++) 
-    {
-        Console.Write($"{array[i]}, ");
-    }
-    Console.Write($"{array[array.Length - 1]}]");
+   Console.Write("[");
+   for (int i = 0; i < array.Length - 1; i++) 
+   {
+      Console.Write($"{array[i]}, ");
+   }
+   Console.Write($"{array[array.Length - 1]}]");
 }
 
 
